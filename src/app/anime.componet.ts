b9e0968fe3195af77c4043594b4412d546f78bf3
JhanAntezana <jhan.antezana6174@gmail.com>
2023-03-10 22:39:29 -0500

    componente anime

---
+++ src/app/anime.componet.ts
@@ -0,0 +1,10 @@
+export interface Anime {
+    name: string,
+    description: string,
+    Rating: number,
+    episode: number,
+    categorie: string,
+    studio: string,
+    img: string
+    characters: string[]
+}
